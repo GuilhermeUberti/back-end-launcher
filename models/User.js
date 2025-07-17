a4227799265f1425c1f89ad5b112159cdcc39ac0
GuilhermeUberti <guilherme.uberti@gmail.com>
2025-07-17 10:09:03 -0300

    checkout

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true },
   password: String,
-  stripeCustomerId: String
+  stripeCustomerId: String,
+  assinatura_ativa: { type: Boolean, default: false } 
 });
 
 module.exports = mongoose.model("User", userSchema);
